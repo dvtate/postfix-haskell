563d0c65f9bb9d95c207e33e124a22abdbcad0a2
DV Tate Testa <toast27@gmail.com>
2020-11-04 22:56:19 -0600

    update types syntax plan

--- examples/types.hs
+++ examples/types.hs
@@ -2,15 +2,15 @@
 -- Still undecided on comments
 
 -- These would probably be defined in standard library
-I32 I64 | Int =
-F32 F64 | Float =
+I32 I64 | $Int =
+F32 F64 | $Float =
 
 -- datatype
-{ Int Float } FloatSequence =
+{ Int Float } newtype $FloatSequence =
 
 -- accessor methods
-{ type FloatSequence == } { destruct pop pop } $.count fun
-{ type FloatSequence == } { destruct pop } $.value fun
+{ typeof FloatSequence == } { destruct pop pop } $.count fun
+{ typeof FloatSequence == } { destruct pop } $.value fun
 
 -- create an instance
 5 3.14 FloatSequence make $seq =
@@ -19,22 +19,26 @@ F32 F64 | Float =
 seq .count -- => 5
 seq .value -- => 3.14
 
-
--- sequence_t is a macro that returns a type
-{ $a =
+-- Sequence is a macro that returns a type
+{ $A =
 	Int
-	a
-} struct sequence_t =
+	A
+} newtype $Sequence =
 
 -- accessor methods
-{ type $a sequence_t == } { destruct pop pop } $.count fun
-{ type $a sequence_t == } { destruct pop } $.value fun
+{ typeof $A Sequence == } { destruct pop pop } $.count fun
+{ typeof $A Sequence == } { destruct pop } $.value fun
 
--- Inheritance
-Float sequence_t FloatSequence =
+-- Aliasing
+Float Sequence FloatSequence =
+
+-- Creating a new type with same structure
+-- Functions that Accept a Sequence type will not accept this new type
+Float Sequence newtype FloatSeq =
+
 
 -- find sum for sequence of values
-{ type $a sequence_t == } {
+{ typeof $A Sequence == } {
 	-- get fields
 	dup .value $value =
 	.count $count =
@@ -45,18 +49,30 @@ Float sequence_t FloatSequence =
 		$v =
 
 		{
-			{ }			{ }						-- == 0 so we have result
-			{ n 0 > }	{ v + v n 1 - iter }	-- > 0 move towards zero
-			{ n 0 < }	{ v - v n 1 + iter }    -- < 0 move towards zero
+			{ }			{ }							-- == 0 so we have result
+			{ n 0 > }	{ v + v 1 + n 1 - iter }	-- > 0 move towards zero
+			{ n 0 < }	{ v - v 1 + n 1 + iter }    -- < 0 move towards zero
 		} cond
 	} $iter =
 	0 $value $count iter
 } $sum fun
 
-
 -- get members and methods
-10 0.5 Float sequence_t make $seq =
+10 0.5 Float Sequence make $seq =
 seq .value -- => 0.5
 seq .count -- => 10
 seq .sum  -- => 5
 
+-- Likely defined in std lib
+I32 I64 | $Int =
+F32 F64 | $Float =
+
+-- make a structure consiting of two member types
+{ Int Float } type $FloatSeq =
+
+--
+{ typeof FloatSequence == } { destruct pop pop } $.count fun
+{ typeof FloatSequence == } { destruct pop } $.value fun
+
+--
+{ $A = Int A } type $Seq =
