4276582c7f62785a3ead3a563b922479fef07ccd
DV Tate Testa <toast27@gmail.com>
2021-01-26 00:32:57 -0600

    good stopping point

--- planning/test.wat
+++ planning/test.wat
@@ -1,3 +1,5 @@
-(module 
+(module
 (func $succ (param $0 i32) (result i32)
-	(i32.add (local.get 0) (i32.const 1))))
+	(i32.add (local.get 0) (i32.const 1)))
+(func $test (param $0 f64) (result f64)
+        (f64.add (f64.add (f64.mul (f64.const 4) (local.get 0)) (f64.const 8)) (f64.mul (f64.const 4) (local.get 0)))))
--- tools/file.js
+++ tools/file.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const lex = require('../lib/scan');
 const parse = require('../lib/parse');
+const error = require('../lib/error');
 
 const fname = process.argv[process.argv.length - 1];
 const src = fs.readFileSync(fname).toString();
@@ -9,19 +10,36 @@ const src = fs.readFileSync(fname).toString();
 // const toks = lex.lex(src);
 // console.log(toks);
 
+// Mock browser API
+const performance = {
+    _hrt: process.hrtime(),
+    now() {
+        return process.hrtime(this._hrt)[1] / 1000000;
+    }
+};
+
+
+// Lex
+let start = performance.now();
 const ptree = lex.parse(src, fname);
-// console.log(ptree);
-// console.log(ptree);
+if (process.env.TRACK_TIME)
+    console.log('lex:', performance.now() - start);
 
-
-
+// Parse (weird meaning here, more like "interpret phase")
+start = performance.now();
 const ctx = parse(ptree);
-const error = require('../lib/error');
-
 if (ctx instanceof error.SyntaxError) {
     const utils = require('./util');
     // console.log(ctx.tokens);
     console.log(utils.formatErrorPos([ctx]));
 }
+if (process.env.TRACK_TIME)
+    console.log('parse:', performance.now() - start);
 
-console.log(ctx.outWast());
+
+start = performance.now();
+ctx.outWast({ folding: false }).then(asm => {
+    console.log(asm);
+    if (process.env.TRACK_TIME)
+        console.log('compile:', performance.now() - start);
+});
